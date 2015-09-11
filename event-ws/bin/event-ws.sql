0912822fdae092eee1fb8fda4272476bffa078b3
hvo <hvo@cisco.com>
2015-09-10 22:26:13 -0700

    fixed users module for postgres

--- event-ws/bin/event-ws.sql
+++ event-ws/bin/event-ws.sql
@@ -2,27 +2,14 @@ USE eventws;
 
 CREATE TABLE Users (
   id BIGSERIAL,
-  phone_number BIGINT NOT NULL UNIQUE,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  reg_status SMALLINT NOT NULL,
-  email VARCHAR(150),
-  created_time TIMESTAMPTZ NOT NULL,
-  last_updated_time TIMESTAMPTZ NOT NULL,
+  data JSONB NOT NULL,
   PRIMARY KEY (id)
 );
 
 
 CREATE TABLE Events (
   id BIGSERIAL,
-  name VARCHAR(255) NOT NULL,
-  theme VARCHAR(255),
-  place VARCHAR(255),
-  time TIMESTAMPTZ NOT NULL,
-  duration INT DEFAULT -1,
-  notes TEXT,
-  created_time TIMESTAMPTZ NOT NULL,
-  last_updated_time TIMESTAMPTZ NOT NULL,
+  data JSONB NOT NULL,
   PRIMARY KEY (id)
 );
 
--- event-ws/lib/users/users_module.js
+++ event-ws/lib/users/users_module.js
@@ -1,97 +1,94 @@
-var regStatus  = require('../users/reg_status_constants');
-var phoneUtils  = require('../users/phone_number_norm');
+var regStatus = require('../users/reg_status_constants');
+var phoneUtils = require('../users/phone_number_norm');
 var moment = require('moment');
 
 var dbAPI = require('../db_layer');
 
 var priv_create_user = function(newUser, response, success_cb, error_cb) {
-  if (!newUser.phone_number) {
-    console.error("Must specify phone_number attribute in user object");
-  } else {
-    newUser.phone_number = phoneUtils.normalize_phone_number(newUser.phone_number);
-    theNow = dbAPI.get_now_ts();
-    newUser.created_time = theNow;
-    newUser.last_updated_time = theNow;
-    console.log("Trying to create a new user/attendee with phone number " + newUser.phone_number);
-    dbAPI.get_db().connect(dbAPI.get_conn_url(), function(err, client, done) {
-        if (err) {
-          console.log("Error getting connection from pool" + err);
-          error_cb(response, err);
-        } else {
-          client.query("INSERT INTO Users(phone_number, last_name, first_name, reg_status, email, created_time, last_updated_time) VALUES($1, $2, $3, $4, $5, $6, %7)", 
-            newUser.phone_number, newUser.last_name, newUser.first_name, newUser.reg_status, newUser.email, 
-            newUser.created_time, newUser.last_updated_time, function(err, result) {
-            done();
+    if (!newUser.phone_number) {
+        console.error("Must specify phone_number attribute in user object");
+    } else {
+        newUser.phone_number = phoneUtils.normalize_phone_number(newUser.phone_number);
+        theNow = dbAPI.get_now_ts();
+        newUser.created_time = theNow;
+        newUser.last_updated_time = theNow;
+        console.log("Trying to create a new user/attendee with phone number " + newUser.phone_number);
+        dbAPI.get_db().connect(dbAPI.get_conn_url(), function(err, client, done) {
             if (err) {
-              error_cb(response, err);
+                console.log("Error getting connection from pool" + err);
+                error_cb(response, err);
             } else {
-              success_cb(response, result.id);
+                client.query("INSERT INTO Users(data) VALUES ('" + JSON.stringify(newUser) + "')", function(err, result) {
+                    done();
+                    if (err) {
+                        error_cb(response, err);
+                    } else {
+                        success_cb(response, result.id);
+                    }
+                });
             }
-          });
-        }
-    });
-  }
+        });
+    }
 }
 
 exports.create_user = function(newUser, response, success_cb, error_cb) {
-  console.log("Creating a user");
-  newUser.reg_status = regStatus.PENDING_VERIFICATION;
-  priv_create_user(newUser, response, success_cb, error_cb);
+    console.log("Creating a user");
+    newUser.reg_status = regStatus.PENDING_VERIFICATION;
+    priv_create_user(newUser, response, success_cb, error_cb);
 }
 
 exports.create_attendee = function(newUser, response, success_cb, error_cb) {
-  console.log("Creating an attendee");
-  newUser.reg_status = regStatus.UNREGISTERED;
-  priv_create_user(newUser, response, success_cb, error_cb);
+    console.log("Creating an attendee");
+    newUser.reg_status = regStatus.UNREGISTERED;
+    priv_create_user(newUser, response, success_cb, error_cb);
 }
 
-exports.get_users_by_phone = function (phoneNumber, response, success_cb, error_cb) {
-	console.log("getting user whose phone number is " + phoneNumber);
+exports.get_users_by_phone = function(phoneNumber, response, success_cb, error_cb) {
+    console.log("getting user whose phone number is " + phoneNumber);
 
-  dbAPI.get_db().connect(dbAPI.get_conn_url(), function(err, client, done) {
-      if (err) {
-        console.log("Error getting connection from pool" + err);
-        error_cb(response, err);
-      } else {
-        client.query("SELECT * FROM Users WHERE phone_number = " + phoneNumber, function(err, result) {
-          done();
-          if (err) {
+    dbAPI.get_db().connect(dbAPI.get_conn_url(), function(err, client, done) {
+        if (err) {
+            console.log("Error getting connection from pool" + err);
             error_cb(response, err);
-          } else {
-            success_cb(response, result.rows);
-          }
-        });
-      }
-  });
-
-
-
-}
-
-exports.get_users_by_id = function (dbId, response, success_cb, error_cb) {
-  console.log("getting user whose db id is " + dbId);
-  dbAPI.get_conn().query("SELECT * FROM Users WHERE id = " + dbId, function(err, result) {
-    if (err) {
-      error_cb(response, err)
-    } else {
-      if (result.length > 0)
-       success_cb(response, result[0]);
-      else 
-        error_cb(response, {message: "No user having user id " + dbId});
-    }
-  });
-}
-
-exports.delete_user = function (phoneNumber, response, success_cb, error_cb) {
-  console.log("deleting user whose phone number is " + phoneNumber);
-  if(phoneNumber) {
-    dbAPI.get_conn().query("DELETE FROM Users WHERE phone_number = " + phoneNumber, function (err, result) {
-      if (err) {
-        error_cb(response, err)
-      } else {
-        // assuming only 1 match
-        success_cb(response, result[0]);
-      }
+        } else {
+            client.query("SELECT * FROM Users WHERE data->>'phone_number' LIKE '" + phoneNumber + "%'", function(err, result) {
+                done();
+                if (err) {
+                    error_cb(response, err);
+                } else {
+                    success_cb(response, result.rows);
+                }
+            });
+        }
     });
-  }
+}
+
+exports.get_users_by_id = function(dbId, response, success_cb, error_cb) {
+    console.log("getting user whose db id is " + dbId);
+    dbAPI.get_conn().query("SELECT * FROM Users WHERE id = " + dbId, function(err, result) {
+        if (err) {
+            error_cb(response, err)
+        } else {
+            if (result.length > 0)
+                success_cb(response, result[0]);
+            else
+                error_cb(response, {
+                    message: "No user having user id " + dbId
+                });
+        }
+    });
+}
+
+exports.delete_user = function(phoneNumber, response, success_cb, error_cb) {
+    console.log("deleting user whose phone number is " + phoneNumber);
+    if (phoneNumber) {
+        dbAPI.get_conn().query("DELETE FROM Users WHERE data->>'phone_number' = '" + phoneNumber + "'", function(err, result) {
+            if (err) {
+                error_cb(response, err)
+            } else {
+                // assuming only 1 match
+                success_cb(response, result[0]);
+            }
+        });
+    }
 }
